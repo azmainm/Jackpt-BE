ac4f7af90fb33c56f5557f8092a2e95325091fa5
foysal_tikweb <rahman@tikweb.com>
2022-11-03 15:43:01 +0600

    remove commented code

--- app/Repository/UserRepository.php
+++ app/Repository/UserRepository.php
@@ -5,36 +5,7 @@ namespace App\Repository;
 use App\Interfaces\UserRepositoryInterface;
 use App\Models\User;
 
-
-//class UserRepository implements UserRepositoryInterface
-//{
-//    public function getAll()
-//    {
-//        return User::all();
-//    }
-//
-//    public function getById($id)
-//    {
-//        return User::findOrFail($id);
-//    }
-//
-//    public function delete($id)
-//    {
-//        User::destroy($id);
-//    }
-//
-//    public function create(array $data)
-//    {
-//        return User::create($data);
-//    }
-//
-//    public function update($id, array $newDetails)
-//    {
-//        return User::whereId($id)->update($newDetails);
-//    }
-//
-//}
- class UserRepository extends BaseRepository implements UserRepositoryInterface
+class UserRepository extends BaseRepository implements UserRepositoryInterface
 {
 
     public function __construct(User $model)
