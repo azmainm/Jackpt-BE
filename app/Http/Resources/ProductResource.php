360a5da28b25931f8fd15bcaa1cbc0e1409335df
foysal_tikweb <rahman@tikweb.com>
2022-11-04 18:46:46 +0600

    create product resource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -17,17 +17,18 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id" => $this->id,
             "title" => $this->title ?? null,
             "user_id" => $this->user_id ?? null,
             "price" => $this->price ?? null,
-            "slug " => $this->slug ?? null,
-            "discounted_price " => $this->discounted_price ?? null,
-            "description " => $this->description ?? null,
-            "variant " => json_decode($this->variant) ?? null,
-            "color " => json_decode($this->color) ?? null,
-            "product_image " => $this->product_image ?? null,
-            "thumbnail_image " => $this->thumbnail_image ?? null,
-            "short_image " => $this->short_image ?? null,
+            "slug" => $this->slug ?? null,
+            "discounted_price" => $this->discounted_price ?? null,
+            "description" => $this->description ?? null,
+            "variant" => json_decode($this->variant) ?? null,
+            "color" => json_decode($this->color) ?? null,
+            "product_image" => $this->product_image ?? null,
+            "thumbnail_image" => $this->thumbnail_image ?? null,
+            "short_image" => $this->short_image ?? null,
         ];
     }
 }
