fc4c557f72b7d9ab447c86587ad2eedc851efe21
Tamanna Akter <tamanna424101@gmail.com>
2023-09-03 22:46:17 +0600

    create issues table and model

---
+++ app/Models/Issue.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use function App\Helpers\storeUuid;
+
+class Issue extends Model
+{
+    use HasFactory;
+
+    protected $table = 'issues';
+
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            storeUuid($model);
+        });
+    }
+    protected $fillable = [
+        'title',
+        'description',
+        'is_solved'
+    ];
+}
