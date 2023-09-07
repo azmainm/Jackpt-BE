b9e816dcbb8a763169eb292783849e888d01f413
Tamanna Akter <tamanna424101@gmail.com>
2023-09-07 20:15:25 +0600

    remove auth from issue

--- app/Http/Controllers/v1/IssueController.php
+++ app/Http/Controllers/v1/IssueController.php
@@ -9,9 +9,11 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class IssueController extends Controller
-{ public function index(): JsonResponse
+{
+    public function view(): JsonResponse
     {
-        $issues = Issue::where('user_id', auth()->user()->id)->get();
+        $issues = Issue::all();
+//        $issues = Issue::where('user_id', auth()->user()->id)->get();
         if ($issues->count() > 0) {
             return $this->success(data: IssueResource::collection($issues));
         }
