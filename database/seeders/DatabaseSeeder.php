2fc9b790d770a3d786c96ca59f96a4d9ad8f6474
Tamanna Akter <tamanna424101@gmail.com>
2023-10-08 21:31:20 +0600

    Post Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             //            CountrySeeder::class,
             UserSeeder::class,
+            PostsSeeder::class,
             //            AnnualWheelRingSeeder::class,
         ]);
     }
